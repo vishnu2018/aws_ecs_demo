fa5097034bbaf83740b1d644497da81388e416fa
guntuka09 <guntuka09@gmail.com>
2022-06-19 22:55:54 -0600

    updated code

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
+'''
+cicd demo
+'''
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world() -> str:
+    '''
+    cicd demo
+    '''
     return 'Hello world! How are you?'
 
 if __name__ == '__main__':
